520d3c0ea020081c548fba8145b5678a387f3830
checoGarcia <illsouth@imac.local>
2021-11-17 09:37:18 -0600

    changes in change password

--- main.py
+++ main.py
@@ -17,7 +17,9 @@ app.config['SECRET_KEY'] = 'expedienteClinicokey'
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
-
+#login_manager.login_view = "login"
+login_manager.login_message = "Necesitas iniciar sesión."
+login_manager.login_message_category = "alert alert-danger"
 #global variables:
 patient_counter = 0
 
@@ -36,9 +38,17 @@ class User(db.Model, UserMixin):
     set_new_password = db.Column(db.Boolean(),nullable=False)
     #the user type will grant certain privileges
     user_type = db.Column(db.String(20),nullable=False)
+    user_cedula = db.Column(db.String(20),nullable=True)
     #default false /// later the admin will accept the user
     active_user = db.Column(db.Boolean(),nullable=False)
+    created_by = db.Column(db.String(80),nullable=False)
     register_date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+
+class Log(db.Model,UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.String(30),unique=False,nullable=False)
+    user_action = db.Column(db.String(200),unique=False,nullable=False)
+    log_date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
 
 class Patient(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -83,10 +93,22 @@ def antecedentes_heredofamiliares():
 @app.route('/manage_accounts',methods=['GET','POST'])
 @login_required
 def manage_accounts():
-    users = User.query.filter_by(active_user=False).all()
-    for user in users:
-        print(user.username)
-
+    users = User.query.all()
+    if request.method == "POST":
+        data = request.form
+        activated = False
+        user_to_modify = User.query.filter_by(username=data["username"]).first()
+        print("this is the name that is going to be modified")
+        print(user_to_modify.username)
+        if data["user_activated"] == "true":
+            activated = True
+            print("------------The user is now activated ------")
+        else:
+            activated = False
+            print("-----The user is DESACTIVATED----")
+        user_to_modify.active_user = activated
+        db.session.commit()
+        flash("Se modifico correctamente","alert alert-success")
     return render_template('manage_accounts.html',users=users)
 
 @app.route('/ingreso_paciente',methods=['GET','POST'])
@@ -112,6 +134,23 @@ def ingreso():
 
     return render_template('login.html')
 
+@app.route('/set_new_password',methods=['GET','POST'])
+@login_required
+def set_new_password():
+    if request.method == 'POST':
+        user = User.query.filter_by(username=current_user.username).first()
+        data = request.form
+        if bcrypt.check_password_hash(current_user.password,data['user_temporal_password']):
+            print('yea this is the password')
+            user.password = bcrypt.generate_password_hash(data['user_new_password'])
+            user.set_new_password = True
+            db.session.commit()
+            log_action(current_user.username,"Ha cambiado de passowrd.")
+            flash("Cambio de contraseña exitoso","alert alert-success")
+            return render_template('dashboard.html')
+
+    return render_template('set_new_password.html')
+
 
 @app.route('/login',methods=['GET','POST'])
 def login():
@@ -127,6 +166,9 @@ def login():
         if user:
             if bcrypt.check_password_hash(user.password,data['password']):
                 login_user(user)
+                if current_user.set_new_password == False:
+                    return render_template('set_new_password.html')
+                log_action(current_user.username,"Inició sesión")
                 return redirect(url_for('dashboard'))
             else:
                 flash("Password incorrecto","alert alert-danger")
@@ -137,14 +179,29 @@ def login():
 @app.route('/logout',methods=['GET','POST'])
 def logout():
     if current_user.is_authenticated:
+        log_action(current_user.username,"Cerro su sesión.")
         logout_user()
         flash("Tu sesión fue cerrada exitosamente.","alert alert-success")
         return redirect(url_for('login'))
 
 @app.route('/dashboard',methods=['GET','POST'])
-@login_required
 def dashboard():
     return render_template('dashboard.html')
+
+#This route is for the log
+@app.route('/log',methods=['GET','POST'])
+@login_required
+def log():
+    if current_user.username == "Administrador":
+        logs = Log.query.limit(100).all()
+        return render_template('log.html',logs=logs)
+
+def log_action(user,user_action):
+    new_enter_log = Log(user=user,
+            user_action = user_action)
+    db.session.add(new_enter_log)
+    db.session.commit()
+
 #This route will take us to two types of register
 @app.route('/register',methods=['GET','POST'])
 @login_required
@@ -158,10 +215,14 @@ def register():
                 password = bcrypt.generate_password_hash(data['password']),
                 user_type= data['user_type'],
                 set_new_password = False,
+                user_cedula = data['user_cedula'],
+                created_by = data['created_by'],
                 active_user = False)
         db.session.add(new_user)
         db.session.commit()
-        return redirect(url_for('login'))
+        log_action(current_user.username,"se creo la cuenta con nombre: %s y con privilegios tipo: %s"%(new_user.username,new_user.user_type))
+        flash('El usuario fue creado correctamente','alert alert-success')
+        return redirect(url_for('dashboard'))
 
     return render_template('register.html')
 
@@ -176,6 +237,7 @@ def register_admin():
                 password = bcrypt.generate_password_hash(data['password']),
                 user_type= data['user_type'],
                 set_new_password = False,
+                created_by = data['created_by'],
                 active_user = False)
         db.session.add(new_user)
         db.session.commit()
